149a0c74f1f856781ad1bb6404f8b113489834c0
A.Fahy <alanfahy87@Alans-MacBook-Air-2.local>
2017-10-26 18:26:33 +0100

    Q1, done

--- assignment_q1.rkt
+++ assignment_q1.rkt
@@ -1,1 +1,6 @@
 #lang racket
+(cons 1 2)
+(cons 1 (cons 2 (cons 3 empty)))
+(cons "Cons"(cons 8 (cons(cons 1 (cons 2 (cons 3 empty)))'())))
+(list "List" 8(list 1 2 3 ))
+(append '("string") '(8) '((567)))
