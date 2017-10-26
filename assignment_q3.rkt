41ba84d97dbdc4947aaa7d19c752646876a90de9
A.Fahy <alanfahy87@Alans-MacBook-Air-2.local>
2017-10-26 23:25:58 +0100

    Question 3.

--- assignment_q3.rkt
+++ assignment_q3.rkt
@@ -1,1 +1,30 @@
 #lang racket
+
+(define binary_tree '(((() 2 ()) 5 (() 7 ())) 10 ((() 12 ()) 15 (() 17 ()))))
+
+(provide display_tree)
+
+(define (display_tree tree)
+  (begin (cond [(not (empty? (car tree)))
+                (display_tree  (car tree))])
+         (display  (cadr tree))(display"\n")
+         (cond [(not (empty? (caddr tree)))
+                (display_tree  (caddr tree))])))
+(display "Binary Tree\n")
+(display_tree  binary_tree)
+
+(provide locate_in_tree)
+(define (locate_in_tree item tree)
+  (cond [(empty? tree) #f]
+        [(equal? item (cadr tree)) #t]
+        [(< item (cadr tree)) (locate_in_tree item (car tree))]
+        [else (locate_in_tree item (caddr tree))]))
+(display "\nLocated\n")
+(locate_in_tree 10 binary_tree)
+
+(provide insert)
+
+
+(provide sort_list)
+
+(provide ascending_order)
